402fcd085246d156a9ed84a230e6a7ade9f1b53f
NDB <blissnd@hotmail.com>
2020-04-27 00:15:07 +0100

    Add subplot capability

--- Generic_Scatter_Plot_Class.py
+++ Generic_Scatter_Plot_Class.py
@@ -1,5 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as parent_plot
+import math
 
 class Graph:
 
@@ -20,8 +21,12 @@ class Graph:
 # End Class
 
 class Generic_Scatter_Plot:
-
-    def __init__(self, plot_title="Scatter Plot"):
+    graph_index = 0
+    
+    def __init__(self, graph_name, plot_title="Scatter Plot"):
+        self.graph_index = Generic_Scatter_Plot.graph_index
+        self.graph_name = graph_name
+        
         self.plot_title = plot_title
         self.x_axis_array = []
         self.graph_dict = {}
@@ -30,9 +35,15 @@ class Generic_Scatter_Plot:
         self.x_axis_label = 'Value'
         self.y_axis_label = 'Result'
         self.num_legend_columns = 1
-         
-        self.fig = plt.figure(figsize=(8,6))
-        self.ax = self.fig.add_subplot(1, 1, 1)
+        
+        self.fig = parent_plot.figure(self.graph_index)
+        
+        self.subplot_index = 0
+        
+        self.gs = None
+        self.plt = None
+        
+        Generic_Scatter_Plot.graph_index = Generic_Scatter_Plot.graph_index + 1
     # End Function
     
     def add_graph(self, graph_name, colour='black', marker_radius=1, marker_shape='.', plot_label=None):
@@ -49,24 +60,59 @@ class Generic_Scatter_Plot:
     
     def plot_graphs(self):
         
-        plt.title(self.plot_title)
-        plt.xlabel(self.x_axis_label)
-        plt.ylabel(self.y_axis_label)
-
+        #self.plt.title(self.plot_title)
+        
+        self.plt.set(xlabel=self.x_axis_label, ylabel=self.y_axis_label)
+        
         for current_graph in self.graph_dict:
         
-            plt.scatter(self.x_axis_array, self.graph_dict[current_graph].y_value_array, s=self.graph_dict[current_graph].marker_size, c=self.graph_dict[current_graph].colour, marker=self.graph_dict[current_graph].marker_shape, label= self.graph_dict[current_graph].plot_label)
+            self.plt.scatter(self.x_axis_array, self.graph_dict[current_graph].y_value_array, s=self.graph_dict[current_graph].marker_size, c=self.graph_dict[current_graph].colour, marker=self.graph_dict[current_graph].marker_shape, label= self.graph_dict[current_graph].plot_label)
           
         # End For
         
-        plt.grid(b=True, which='major', color='#444444', linestyle='-', alpha=0.2)
-        plt.minorticks_on()
-        plt.grid(b=True, which='minor', color='#AAAAAA', linestyle='-', alpha=0.2)
+        self.plt.grid(b=True, which='major', color='#444444', linestyle='-', alpha=0.2)
+        self.plt.minorticks_on()
+        self.plt.grid(b=True, which='minor', color='#AAAAAA', linestyle='-', alpha=0.2)
 
-        plt.legend(loc=self.legend_position, bbox_to_anchor=self.legend_margin, fontsize='small', ncol=self.num_legend_columns)
-        plt.tight_layout()
-        plt.show()
+        self.plt.legend(loc=self.legend_position, bbox_to_anchor=self.legend_margin, fontsize='small', ncol=self.num_legend_columns)
+        parent_plot.tight_layout()
         
     # End Function
+    
+# End Class
+
+class Graph_Window_Class():
+    
+    graphs = {}
+    
+    def add_subplot(graph_name, subplot_name):
+    
+        if graph_name not in Graph_Window_Class.graphs:
+             Graph_Window_Class.graphs[graph_name] = {}
+        # End If
+        
+        Graph_Window_Class.graphs[graph_name][subplot_name] = Generic_Scatter_Plot(graph_name, plot_title="Scatter Plot")
+        
+        Graph_Window_Class.graphs[graph_name][subplot_name].gs = Graph_Window_Class.graphs[graph_name][subplot_name].fig.add_gridspec(1, 1)
+        Graph_Window_Class.graphs[graph_name][subplot_name].plt = Graph_Window_Class.graphs[graph_name][subplot_name].fig.add_subplot(Graph_Window_Class.graphs[graph_name][subplot_name].gs[0, 0])
+        
+        #gs = self.fig.add_gridspec(2, 2)        
+        #self.plt2 = self.fig.add_subplot(gs[1, 0])
+        #self.plt3 = self.fig.add_subplot(gs[0, 1])
+        #self.plt4 = self.fig.add_subplot(gs[1, 1])
+        
+    # End Static Function
+    
+    def render_subplots():
+    
+        for graph_name, graph_object in Graph_Window_Class.graphs.items():
+            print(len(graph_object))
+        # End For
+        
+    # End Static Function
+    
+    def show_plots():
+        parent_plot.show()
+    # End Static Function
     
 # End Class
--- python_plot_example.py
+++ python_plot_example.py
@@ -8,23 +8,40 @@ a_column = [60,70,80,90,100]
 b_column = [110,120,130,140,150]
 c_column = [210,220,230,240,250]
 
-generic_scatter_plot_obj = Generic_Scatter_Plot()
+###############################################################################################################################
+def plot_standalone_graph(graph_name, subplot_name):
+    generic_scatter_plot_obj = Graph_Window_Class.add_subplot(graph_name, subplot_name)
 
-generic_scatter_plot_obj.add_graph('One', colour='red', marker_radius=2, plot_label='Graph One')
-generic_scatter_plot_obj.add_graph('Two', colour='green', marker_radius=2, marker_shape="^", plot_label='Graph Two')
-generic_scatter_plot_obj.add_graph('Three', colour='blue', marker_radius=2, marker_shape="x", plot_label='Graph Three')
+    Graph_Window_Class.graphs[graph_name][subplot_name].add_graph('One', colour='red', marker_radius=2, plot_label='Graph One')
+    Graph_Window_Class.graphs[graph_name][subplot_name].add_graph('Two', colour='green', marker_radius=2, marker_shape="^", plot_label='Graph Two')
+    Graph_Window_Class.graphs[graph_name][subplot_name].add_graph('Three', colour='blue', marker_radius=2, marker_shape="x", plot_label='Graph Three')
 
-generic_scatter_plot_obj.legend_margin = (1.3, 1)
+    Graph_Window_Class.graphs[graph_name][subplot_name].legend_margin = (1.4, 1)
 
-counter = 0
-while counter < len(x_column):
-    generic_scatter_plot_obj.append_x_axis_val(x_column[counter])
+    counter = 0
+    while counter < len(x_column):
+        Graph_Window_Class.graphs[graph_name][subplot_name].append_x_axis_val(x_column[counter])
+        
+        Graph_Window_Class.graphs[graph_name][subplot_name].append_y_val_to_graph('One', a_column[counter])
+        Graph_Window_Class.graphs[graph_name][subplot_name].append_y_val_to_graph('Two', b_column[counter])    
+        Graph_Window_Class.graphs[graph_name][subplot_name].append_y_val_to_graph('Three', c_column[counter])
+        
+        counter = counter + 1
+    # End While
+
+    Graph_Window_Class.render_subplots()
+    Graph_Window_Class.graphs[graph_name][subplot_name].plot_graphs()
     
-    generic_scatter_plot_obj.append_y_val_to_graph('One', a_column[counter])
-    generic_scatter_plot_obj.append_y_val_to_graph('Two', b_column[counter])    
-    generic_scatter_plot_obj.append_y_val_to_graph('Three', c_column[counter])
-    
-    counter = counter + 1
-# End While
+# End Function
+###############################################################################################################################
 
-generic_scatter_plot_obj.plot_graphs()
+plot_standalone_graph("One", ".1")
+plot_standalone_graph("Two", ".2")
+plot_standalone_graph("Three", ".3")
+plot_standalone_graph("Four", ".4")
+
+###############################################################################################################################
+
+Graph_Window_Class.show_plots() 
+
+###############################################################################################################################
